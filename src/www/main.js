d732a96e8290b2cc70decd84c6a4cbdf4560dbb5
jalotra <jalotrashivam9@gmail.com>
2023-11-04 09:59:48 +0530

    changed IP to localhost

--- src/www/main.js
+++ src/www/main.js
@@ -4,7 +4,7 @@ const author = document.getElementById('author');
 
 let clientName = window.location.pathname.split("/")[1]
 
-fetch(`http://3.84.52.187:80/motivate/${clientName}`)
+fetch(`http://localhost:80/motivate/${clientName}`)
         .then(res => res.json())
         .then(data => {
             message.innerText = data.quote;
